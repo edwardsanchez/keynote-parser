37a91f27b3736307b24358e2b0827f2fd5045679
Edward Sanchez <edwardsanchez@mac.com>
2026-02-21 14:11:19 -0800

    Adding xcode project

---
+++ Keynote Outliner/Keynote Outliner/Keynote_OutlinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keynote_OutlinerApp.swift
+//  Keynote Outliner
+//
+//  Created by Edward Sanchez on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Keynote_OutlinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
